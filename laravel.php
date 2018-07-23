99596b7cdc93fa872805878d120b5b408f5e1cc5
cesarbash <39383016+cesarbash@users.noreply.github.com>
2018-07-23 14:38:26 -0500

    Update laravel.php

--- laravel.php
+++ laravel.php
@@ -1,5 +1,7 @@
 //create query using ::with multiple with conditions
 
-$questionnaires = Questionnaire::with( [ 'questions.answers' => function( $query ) use ($store,$date) {
-          $query->where( 'store_id', $store )->where('numeration_date',$date);
-        }])->get();
+$questionnaires = Questionnaire::with(['questions' => function($query) use ($questionnaire) {
+            $query->where('id',$questionnaire); 
+}, 'questions.answers' => function($query) use ($store,$date) {
+  $query->where( 'store_id', $store )->where('numeration_date',$date);
+}])->get();
